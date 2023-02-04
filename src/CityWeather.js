76d08a7374f138592eb0b3f37e75d718b84c72e0
Lucca Araujo <lucca.ara7@gmail.com>
2023-02-04 12:40:48 -0800

    Set up first files

---
+++ src/CityWeather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from 'axios';
+
+function CityWeather(props){
+    return(
+        <div>
+            city Weather
+        </div>
+    )
+}
+
+export default CityWeather;
---
+++ src/WeatherApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function WeatherApp(props){
+    return(<div>
+        hello world!
+    </div>)
+}
+
+export default WeatherApp;
