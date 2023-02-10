2d136d317503d94d612cd5e3b9b6369ca4fa1a69
Lucca Araujo <lucca.ara7@gmail.com>
2023-02-10 12:56:33 -0800

    added design layout

--- src/CityWeather.js
+++ src/CityWeather.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import './css/utilities.css';
+import './css/weather.css';
 
 function CityWeather(props){
-    console.log(props.cityName);
+
+    const [cityData, changeCityData] = useState({})
+
+    useEffect(()=> {
+        const fetchWeather = async() =>{
+            var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.cityName}&appid=eb1f5528e093e46e1671d62eca019668&units=imperial`;
+            const response = await axios.get(apiUrl);
+            console.log(response.data);
+            changeCityData(response.data)
+        }
+        fetchWeather()
+    }, [props.cityName])
+
+    if(!cityData.weather){
+        return(<h1>Loading...</h1>)
+    }
+
+    const iconUrl = `http://openweathermap.org/img/w/${cityData.weather[0].icon}.png`
+
+
+
     return(
         <div>
-            <h1>{props.cityName}</h1>
-         
+            {cityData &&
+            <div className="con">
+                <div className="city-name">{cityData.name}</div>
+                <div className="temp">{cityData.main.temp}</div>
+                <img src={iconUrl} />
+
+                <div>
+                  feels like:  {cityData.main.feels_like}
+                </div>
+
+                {/* Max and Min temperature */}
+                <div className="s-container">
+                    <div>
+                    Max:  {cityData.main.temp_max}
+                    </div>
+
+                    <div>
+                    Min:  {cityData.main.temp_min}
+                    </div>
+                </div>
+            </div>
+                  }
+        
         </div>
+       
     )
 }
 
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import CityWeather from './CityWeather';
+import './css/utilities.css';
+import './css/weather.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular, brands, icon, light } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 function WeatherApp(props){
     const [city, changeCity] = useState("Toronto");
@@ -10,16 +14,25 @@ function WeatherApp(props){
         changeCityNameWeather(city)
     }
 
+    const searchIcon = <FontAwesomeIcon icon={solid('magnifying-glass')} />
+
+
     return(
-    <div className='container'>
-        <CityWeather cityName={cityNameWeather} />
-        <div>
-            <form onSubmit={searchCity}>
-                <input type="text" value={city} onChange={(e)=>changeCity(e.target.value)} />
-                 <input type="submit" value="search" />
-            </form>
+    <div className='wrapper flex bg-dark vh-100 p-5'>
+        <div className='background-img'>
+            <div className='container-1 glass-effect p-2'>
+                    <form className='form-box' onSubmit={searchCity}>
+                        <input type="text" value={city} onChange={(e)=>changeCity(e.target.value)} />
+                        <button type="submit" className='btn'>
+                            {searchIcon}
+                        </button>
+                    </form>
+
+                    <CityWeather cityName={cityNameWeather} />
+            </div>
         </div>
-    </div>)
+    </div>
+    )
 }
 
 export default WeatherApp;
